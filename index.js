d5611db389babed2a6802a0bcd6c6617bc9a1978
Harum Helmy <harumendhah@gmail.com>
2019-08-09 12:06:15 -0400

    thank you @lesliesage for helping me understand FETCH

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
+  
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
   const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
-  let joke;
+  let joke
 
   function fetchJoke(){
     fetch('https://icanhazdadjoke.com/', {
-      headers: {
-        "Accept": "application/json"
-      }
+      headers: {"Accept": "application/json"}
     })
     .then(res => res.json())
     .then(jokeData => joke = jokeData.joke)
   }
+  
+  fetchJoke()
 
   form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    
+    const username = document.getElementById("name-input").value
 
-    if(username === "") return;
-    fetchJoke()
+    if(!username === "") return;
     newJokeLi.innerHTML = `
     <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
+      `
+    jokeList.appendChild(newJokeLi);
+
+    fetchJoke()
   })
+
+  
+
+
+
+    
+
 })
+
+  
+  
